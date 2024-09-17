6f3ba4313fc2e4d881ad2c12a2c620835b5ed5b9
Zaviyar Ansari <zawiyaransari5@gmail.com>
2024-09-17 10:41:35 +0500

    update

--- initial-pro/src/components/Product.jsx
+++ initial-pro/src/components/Product.jsx
@@ -45,9 +45,9 @@ const Product = () => {
                   </div>
 
                   <div>
-                      <h2 class="text-2xl font-bold text-gray-800">Product Name</h2>
+                      <h2 class="text-2xl font-bold text-gray-800 text-left">Product Name</h2>
                       
-                      <div class="mt-8">
+                      <div class="mt-8 border-b-2 border-gray-300 text-left p-5">
                           <h3 class="text-xl font-bold text-gray-800">Category: Pizza</h3>
                           <ul class="space-y-3 list-disc mt-4 pl-4 text-sm text-gray-800">
                             <p>
@@ -59,8 +59,8 @@ risus. Lacus nisi, et ac dapibus sit eu velit in consequat
                           </ul>
                       </div>
                       <div class="flex flex-wrap gap-4 mt-4">
-                          <p class="text-gray-800 text-xl font-bold">$12</p>
-                          <p class="text-gray-400 text-xl"><strike>$16</strike> <span class="text-sm ml-1.5">Tax included</span></p>
+                          <p class="text-gray-800 text-3xl font-bold">$12</p>
+
                       </div>
 
                       <div class="flex space-x-2 mt-4">
@@ -89,19 +89,25 @@ risus. Lacus nisi, et ac dapibus sit eu velit in consequat
                               <path
                                   d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
                           </svg>
+                          <p>| 5.0 Rating | 22 Review</p>
                       </div>
 
-                      <div class="mt-8">
-                          <h3 class="text-xl font-bold text-gray-800">Sizes</h3>
-                          <div class="flex flex-wrap gap-4 mt-4">
-                              <button type="button" class="w-10 h-10 border-2 hover:border-blue-600 font-semibold text-sm rounded-full flex items-center justify-center shrink-0">SM</button>
-                              <button type="button" class="w-10 h-10 border-2 hover:border-blue-600 border-blue-600 font-semibold text-sm rounded-full flex items-center justify-center shrink-0">MD</button>
-                              <button type="button" class="w-10 h-10 border-2 hover:border-blue-600 font-semibold text-sm rounded-full flex items-center justify-center shrink-0">LG</button>
-                              <button type="button" class="w-10 h-10 border-2 hover:border-blue-600 font-semibold text-sm rounded-full flex items-center justify-center shrink-0">XL</button>
-                          </div>
+                      <div class="mt-8 text-left">
+                          <p>Quantity</p>
                       </div>
+                      <div class="flex items-center border-b-2 border-gray-300 space-x-10 p-5">
+ 
+  <div class="flex items-center border-2 w-28 h-10 rounded-lg text-center justify-center">
+   
+    <button type="button" class="w-10 h-full bg-gray-200 hover:bg-gray-300 text-lg font-semibold">-</button>
+    <span class="w-12 text-lg font-semibold">1</span>
+    <button type="button" class="w-10 h-full bg-gray-200 hover:bg-gray-300 text-lg font-semibold">+</button>
+  </div>
 
-                      <button type="button" class="w-full mt-8 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white text-sm font-semibold rounded-md">Add to cart</button>
+  <button type="button" class="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white text-sm font-semibold rounded-md">
+    Add to cart
+  </button>
+</div>
 
 
 
@@ -109,7 +115,40 @@ risus. Lacus nisi, et ac dapibus sit eu velit in consequat
               </div>
           </div>
       </div>
+      <div class="mt-8">
+            <ul class="flex border-b">
+              <li
+                class="text-gray-800 font-bold text-sm bg-gray-100 py-3 px-8 border-b-2 border-gray-800 cursor-pointer transition-all">
+                Description</li>
+              <li class="text-gray-600 font-bold text-sm hover:bg-gray-100 py-3 px-8 cursor-pointer transition-all">Reviews</li>
+            </ul>
+            <div class="mt-8 text-left">
+              <h3 class="text-lg font-bold text-gray-800">Description
+              </h3>
+              <p>
+              Nam tristique porta ligula, vel viverra sem eleifend nec. Nulla sed purus augue, eu euismod tellus. Nam mattis eros nec mi sagittis sagittis. Vestibulum suscipit cursus bibendum. Integer at
+justo eget sem auctor auctor eget vitae arcu. Nam tempor malesuada porttitor. Nulla quis dignissim ipsum. Aliquam pulvinar iaculis justo, sit amet interdum sem hendrerit vitae. Vivamus
+vel erat tortor. Nulla facilisi. In nulla quam, lacinia eu aliquam ac, aliquam in nisl.
+              </p>
+              <p>
+              Suspendisse cursus sodales placerat. Morbi eu lacinia ex. Curabitur blandit justo urna, id porttitor est dignissim nec. Pellentesque scelerisque hendrerit posuere. Sed at dolor quis nisi rutrum
+accumsan et sagittis massa. Aliquam aliquam accumsan lectus quis auctor. Curabitur rutrum massa at volutpat placerat. Duis sagittis vehicula fermentum. Integer eu vulputate justo.
+Aenean pretium odio vel tempor sodales. Suspendisse eu fringilla leo, non aliquet sem.
+              </p>
+            </div>
+            <div class="mt-8">
+              <h3 class="text-lg font-bold text-gray-800 text-left">Key Benefits
+              </h3>
+              
+            </div>
 
+            <ul class="space-y-3 list-none	text-left mt-6 pl-4 text-sm text-gray-600 ">
+              <li>A pair of gray shoes is a wardrobe essential due to its versatility.</li>
+              <li>Available in a wide range of sizes, from extra small to extra large, and even in tall and petite sizes.</li>
+              <li>Easy to maintain, they can be machine-washed and dried on low heat.</li>
+              <li>Personalize them with your own designs, patterns, or embellishments to make them uniquely yours.</li>
+            </ul>
+          </div>
     </div>
   )
 }
